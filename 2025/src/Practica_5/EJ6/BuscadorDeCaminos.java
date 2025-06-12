1009f1c9fe21dc5638275fa74bf7f33255aed1b8
Fede Dobal <dobalfede@gmail.com>
2025-06-11 23:27:59 -0300

    .

--- 2025/src/Practica_5/EJ6/BuscadorDeCaminos.java
+++ 2025/src/Practica_5/EJ6/BuscadorDeCaminos.java
@@ -2,11 +2,14 @@ package Practica_5.EJ6;
 
 import Practica_5.EJ1.Graph;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BuscadorDeCaminos {
     private Graph<String> bosque;
-    public List<List<String>> recorridosMasSeguro() {
+    public List<List<String>> recorridosMasSeguros() {
+        List<List<String>> ret = new LinkedList<>();
         
+        return ret;
     }
 }
